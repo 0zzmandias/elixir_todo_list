8bebc16b9e6f7da6ec4b42c84b762d4a5df0c9b9
Helton Alves <helton.alves.sa@gmail.com>
2025-12-14 21:08:02 -0300

    Fase 2: Lógica em memória - Exibição de lista estática

--- lib/elixir_todo_list_web/live/todo_live.ex
+++ lib/elixir_todo_list_web/live/todo_live.ex
@@ -1,5 +1,16 @@
 defmodule ElixirTodoListWeb.TodoLive do
   use ElixirTodoListWeb, :live_view
-  def mount(_P, _S, socket), do: {:ok, socket}
-  def render(assigns), do: ~H"<h1>Fase 1: Hello World</h1>"
+
+  def mount(_P, _S, socket),
+    do: {:ok, assign(socket, tasks: [%{title: "Tarefa 1"}, %{title: "Tarefa 2"}])}
+
+  def render(assigns) do
+    ~H"""
+    <ul>
+      <%= for t <- @tasks do %>
+        <li>{t.title}</li>
+      <% end %>
+    </ul>
+    """
+  end
 end
