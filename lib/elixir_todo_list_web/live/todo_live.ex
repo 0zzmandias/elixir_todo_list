b358e130b5dc45efccf658f6177007e966235d95
Helton Alves <helton.alves.sa@gmail.com>
2025-12-14 21:09:51 -0300

    Fase 4: Integração LiveView com Ecto (Repo)

--- lib/elixir_todo_list_web/live/todo_live.ex
+++ lib/elixir_todo_list_web/live/todo_live.ex
@@ -1,11 +1,23 @@
 defmodule ElixirTodoListWeb.TodoLive do
   use ElixirTodoListWeb, :live_view
+  alias ElixirTodoList.{Repo, Task}
+  import Ecto.Query
 
-  def mount(_P, _S, socket),
-    do: {:ok, assign(socket, tasks: [%{title: "Tarefa 1"}, %{title: "Tarefa 2"}])}
+  def mount(_P, _S, socket) do
+    tasks = Repo.all(from t in Task, order_by: [desc: t.inserted_at])
+    {:ok, assign(socket, tasks: tasks, form: to_form(Task.changeset(%Task{}, %{})))}
+  end
+
+  def handle_event("save", %{"task" => p}, socket) do
+    %Task{} |> Task.changeset(p) |> Repo.insert()
+    {:noreply, assign(socket, tasks: Repo.all(from t in Task, order_by: [desc: t.inserted_at]))}
+  end
 
   def render(assigns) do
     ~H"""
+    <.form for={@form} phx-submit="save">
+      <.input field={@form[:title]} /><button>Salvar</button>
+    </.form>
     <ul>
       <%= for t <- @tasks do %>
         <li>{t.title}</li>
